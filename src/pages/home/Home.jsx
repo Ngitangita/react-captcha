cead42bde20551d1b526f0985b941533b8071e35
Ngitangita <hei.elise.3@gmail.com>
2024-12-18 15:51:33 +0300

    title

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,33 @@
 import { useCaptcha } from "../../hooks";
 
 export default function Home() {
-     const captchaAxios = useCaptcha();
-     const showCaptcha = () => {
-        console.log("SHOW CAPTCHA");
-        
-        captchaAxios
-          .get("https://jsonplaceholder.typicode.com/posts/1") 
-          .then((response) => {
-            console.log("API Response:", response.data);
-          })
-          .catch((error) => {
-            console.error("Erreur Captcha ou autre :", error);
-          });
+  const captchaAxios = useCaptcha();
+  const showCaptcha = () => {
+    console.log("SHOW CAPTCHA");
 
-          captchaAxios.renderCaptcha().then((wafToken) => {
-            console.log("CAPTCHA token reçu:", wafToken);
-          }).catch((error) => {
-            console.error("Erreur lors du rendu du CAPTCHA", error);
-          });
-      };
+    captchaAxios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => {
+        console.log("API Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur Captcha ou autre :", error);
+      });
 
+    captchaAxios
+      .renderCaptcha()
+      .then((wafToken) => {
+        console.log("CAPTCHA token reçu:", wafToken);
+      })
+      .catch((error) => {
+        console.error("Erreur lors du rendu du CAPTCHA", error);
+      });
+  };
 
   return (
     <div>
+      <p>Login with CAPTCHA</p>
       <button onClick={showCaptcha}>Afficher Capcha</button>
     </div>
-  )
+  );
 }
